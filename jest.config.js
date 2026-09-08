ed2edce17a6e901be20b414bc637f4053631aa08
steniosousa <steniosousaf@gmail.com>
2026-09-08 13:54:24 -0300

    test: config jest

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+export default {
+  testEnvironment: "node",
+
+  transform: {
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        useESM: true,
+      },
+    ],
+  },
+
+  extensionsToTreatAsEsm: [".ts"],
+
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
+};
