369520aa5b246c7ea4322c548ed6523fb70a1ed1
steniosousa <steniosousaf@gmail.com>
2026-09-09 14:56:30 -0300

    fix: count in log

--- src/terminalConfig.ts
+++ src/terminalConfig.ts
@@ -1,29 +1,33 @@
 import pc from "picocolors";
 
-export function consoleCheckout(logs: { env: string, status: "ok" | "warn" | "error" | "info" }[],baseDir:string) {
-    console.log(pc.bold(pc.cyan("env-lib-check")) + " v1.0.0");
-    console.log(`Scanning: ${baseDir}`);
-    console.log("");
-    if(logs.length === 0){
-        console.log(pc.red(`NO ENVIROMENT VARIABLES NOT FOUND IN YOUR PROJECT`));
-        return
+export function consoleCheckout(
+  logs: { env: string; status: "ok" | "warn" | "error" | "info" }[],
+  baseDir: string,
+) {
+  console.log(pc.bold(pc.cyan("env-lib-check")) + " v1.0.0");
+  console.log(`Scanning: ${baseDir}`);
+  console.log("");
+  if (logs.length === 0) {
+    console.log(pc.red(`NO ENVIROMENT VARIABLES NOT FOUND IN YOUR PROJECT`));
+    return;
+  }
+
+  logs.forEach((log) => {
+    if (log.status === "error") {
+      console.log(pc.red(`${log.env} NOT FOUND IN .ENV`));
+    } else if (log.status === "warn") {
+      console.log(pc.yellow(`${log.env} SET BUT BLANK`));
+    } else if (log.status === "info") {
+      console.log(pc.blue(`${log.env}`));
+    } else {
+      console.log(pc.green(`${log.env} CORRECT ✅`));
     }
-
-    logs.forEach((log) => {
-        if (log.status === "error") {
-            console.log(pc.red(`${log.env} NOT FOUND IN .ENV`));
-        } else if (log.status === "warn") {
-            console.log(pc.yellow(`${log.env} SET BUT BLANK`));
-        } else if(log.status === "info") {
-            console.log(pc.blue(`${log.env}`));
-        } else {
-            console.log(pc.green(`${log.env} CORRECT ✅`));
-        }
-    })
-    console.log("");
-    const ok = logs.filter((log) => log.status === "ok")
-    const error = logs.filter((log) => log.status === "error")
-    const warn = logs.filter((log) => log.status === "warn")
-    console.log(`${ok} passed, ${warn} warnings, ${error} errors`);
-
+  });
+  console.log("");
+  const ok = logs.filter((log) => log.status === "ok");
+  const error = logs.filter((log) => log.status === "error");
+  const warn = logs.filter((log) => log.status === "warn");
+  console.log(
+    `${ok.length} passed, ${warn.length} warnings, ${error.length} errors`,
+  );
 }
